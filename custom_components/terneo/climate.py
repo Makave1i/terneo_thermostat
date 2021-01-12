61607e6b75569ccb0952cbf8433109f55b319f04
Makave1i <makave1i@me.com>
2021-01-12 07:37:17 +0200

    HACS integration

---
+++ custom_components/terneo/climate.py
@@ -0,0 +1,163 @@
+"""Terneo Thermostat Support."""
+import logging
+
+from .thermostat import Thermostat
+import requests
+import voluptuous as vol
+from typing import Optional
+
+from homeassistant.components.climate import PLATFORM_SCHEMA, ClimateEntity
+from homeassistant.components.climate.const import (
+    CURRENT_HVAC_HEAT,
+    CURRENT_HVAC_IDLE,
+    CURRENT_HVAC_OFF,
+    HVAC_MODE_AUTO,
+    HVAC_MODE_HEAT,
+    HVAC_MODE_OFF,
+    SUPPORT_TARGET_TEMPERATURE,
+)
+from homeassistant.const import (
+    ATTR_TEMPERATURE,
+    CONF_HOST,
+    CONF_NAME,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_USERNAME,
+    TEMP_CELSIUS,
+)
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_SERIAL = "serial"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_SERIAL): cv.string,
+        vol.Required(CONF_HOST): cv.string,
+        vol.Optional(CONF_NAME, default="Terneo"): cv.string,
+        vol.Optional(CONF_PORT, default=80): cv.port,
+        vol.Inclusive(CONF_USERNAME, "authentication"): cv.string,
+        vol.Inclusive(CONF_PASSWORD, "authentication"): cv.string,
+    }
+)
+
+SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE
+SUPPORT_HVAC = [HVAC_MODE_AUTO, HVAC_MODE_HEAT, HVAC_MODE_OFF]
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Terneo platform."""
+    serialnumber = config.get(CONF_SERIAL)
+    name = config.get(CONF_NAME)
+    host = config.get(CONF_HOST)
+    port = config.get(CONF_PORT)
+    username = config.get(CONF_USERNAME)
+    password = config.get(CONF_PASSWORD)
+
+    try:
+        therm = Thermostat(serialnumber, host, port=port, username=username, password=password)
+    except (ValueError, AssertionError, requests.RequestException):
+        return False
+
+    add_entities((ThermostatDevice(therm, name),), True)
+
+
+class ThermostatDevice(ClimateEntity):
+    """Interface class for the thermostat module."""
+
+    def __init__(self, thermostat, name):
+        """Initialize the device."""
+        self._name = name
+        self.thermostat = thermostat
+
+        # set up internal state varS
+        self._state = None
+        self._temperature = None
+        self._setpoint = None
+        self._mode = None
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return SUPPORT_FLAGS
+
+    @property
+    def hvac_mode(self):
+        """Return hvac operation ie. heat, cool mode.
+        Need to be one of HVAC_MODE_*.
+        """
+
+        if self._mode == -1:
+            return HVAC_MODE_OFF
+        if self._mode == 3:
+            return HVAC_MODE_HEAT
+        return HVAC_MODE_AUTO
+
+    @property
+    def hvac_modes(self):
+        """Return the list of available hvac operation modes.
+        Need to be a subset of HVAC_MODES.
+        """
+        return SUPPORT_HVAC
+
+    @property
+    def name(self):
+        """Return the name of this Thermostat."""
+        return self._name
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement used by the platform."""
+        return TEMP_CELSIUS
+
+    @property
+    def hvac_action(self):
+        """Return current hvac i.e. heat, cool, idle."""
+        if self._mode == -1:
+            return CURRENT_HVAC_OFF
+        if self._state:
+            return CURRENT_HVAC_HEAT
+        return CURRENT_HVAC_IDLE
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        return self._temperature
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return self._setpoint
+
+    @property
+    def target_temperature_step(self) -> Optional[float]:
+        """Return the supported step of target temperature."""
+        return 1.0
+
+    @property
+    def unique_id(self):
+        """Return unique ID based on Terneo serial number."""
+        return self.thermostat.sn
+
+    def set_hvac_mode(self, hvac_mode):
+        """Set new target hvac mode."""
+        if hvac_mode == HVAC_MODE_AUTO:
+            self.thermostat.mode = 0
+        elif hvac_mode == HVAC_MODE_HEAT:
+            self.thermostat.mode = 1
+        elif hvac_mode == HVAC_MODE_OFF:
+            self.thermostat.turn_off()
+
+    def set_temperature(self, **kwargs):
+        """Set the temperature."""
+        temp = kwargs.get(ATTR_TEMPERATURE)
+        self.thermostat.setpoint = temp
+
+    def update(self):
+        """Update local state."""
+        self.thermostat.update()
+        self._setpoint = self.thermostat.setpoint
+        self._temperature = self.thermostat.temperature
+        self._state = self.thermostat.state
+        self._mode = self.thermostat.mode
---
+++ custom_components/terneo/thermostat.py
@@ -0,0 +1,236 @@
+import sys
+import requests
+import logging
+import time
+
+from requests.auth import HTTPBasicAuth
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Thermostat:
+    """
+    A class for interacting with the Terneo Thermostat's HTTP API.
+    Parameters
+    ----------
+    serialnumber: `str`
+        Serial Number of device
+    host : `str`
+        Hostname or IP address.
+    port : `int` (optional)
+        The port of the web server.
+    username : `str` (optional)
+        The username for HTTP auth.
+    password : `str` (optional)
+        The password for the HTTP auth.
+    """
+
+    def __init__(self, serialnumber, host, port=80, username=None, password=None):
+        if username or password and not username and password:
+            raise ValueError(
+                "Username and Password must both be specified, if either are specified."
+            )
+        elif username or password:
+            self.auth = HTTPBasicAuth(username, password)
+        else:
+            self.auth = None
+
+        self.sn = serialnumber
+
+        self._base_url = "http://{}:{}/{{endpoint}}.cgi".format(host, port)
+        self._setpoint = None
+        self._temperature = None
+        self._mode = None
+        self._state = None
+
+        self._last_request = time.time()
+
+        try:
+            r = requests.get(self._base_url.format(endpoint="api.html")[:-4])
+            assert r.status_code == 200
+        except Exception as e:
+            raise type(e)("Connection to Thermostat failed with: {}".format(str(e))).with_traceback(sys.exc_info()[2])
+
+    def _get_url(self, endpoint):
+        return self._base_url.format(endpoint=endpoint)
+
+    def get(self, endpoint, **kwargs):
+        """
+        Perform a GET request
+        Parameters
+        ----------
+        endpoint : `str`
+            The endpoint to send the request to, will have 'cgi' appended to it.
+        kwargs : `dict`
+            All other kwargs are passed to `requests.get`
+        Returns
+        -------
+        response : `requests.Response`
+            The result of the request.
+        """
+        kwergs = {'auth': self.auth}
+        kwergs.update(kwargs)
+
+        r = requests.get(self._get_url(endpoint), **kwergs)
+        return r
+
+    def post(self, endpoint='api', **kwargs):
+        """
+        Perform a POST request
+        Parameters
+        ----------
+        endpoint : `str`
+            The endpoint to send the request to, will have 'cgi' appended to it.
+        kwargs : `dict`
+            All other kwargs are passed to `requests.post`
+        Returns
+        -------
+        response : `requests.Response`
+            The result of the request.
+        """
+        kwergs = {'auth': self.auth}
+
+        kwergs.update(kwargs)
+
+        start_time = time.time()
+        if start_time - self._last_request < 1:
+            time.sleep(1)
+
+        # _LOGGER.error(f"terneo request start time: {start_time}. cmd - {kwargs['json'].get('cmd')}; pars - {kwargs['json'].get('par')}")
+        try:
+            r = requests.post(self._get_url(endpoint), timeout=5, **kwergs)
+        except Exception as e:
+            self._last_request = time.time()
+            _LOGGER.error(e)
+            return False
+        end_time = time.time()
+        # _LOGGER.error(f'terneo request end time: {end_time}. diff: {end_time - start_time}')
+        self._last_request = end_time
+        content = r.json()
+
+        if content.get('status', '') == 'timeout':
+            _LOGGER.error(f'terneo timout: {kwargs}')
+            return False
+
+        return content
+
+    def status(self):
+        """
+        Get the status dictionary from the thermostat
+        """
+        r = self.post(json={"cmd": 4})
+        return r
+
+    def is_on(self):
+        """
+        getting power on/off for firmware 2.3
+        :return: bool
+        """
+        r = self.post(json={"cmd": 1})
+        if r and 'par' in r:
+            for a in r['par']:
+                if a[0] == 125:
+                    return a[2] == "0"
+        return False
+
+    @property
+    def temperature(self):
+        """
+        Current value of the temperature sensor in C
+        """
+        if self._temperature is None:
+            data = self.status()
+            if data:
+                self._temperature = self.get_temperature(data)
+        return self._temperature
+
+    @staticmethod
+    def get_temperature(data):
+        return float(data['t.1']) / 16
+
+    @property
+    def setpoint(self):
+        """
+        Current thermostat setpoint in C
+        """
+        if self._setpoint is None:
+            data = self.status()
+            if data:
+                self._setpoint = self.get_setpoint(data)
+        return self._setpoint
+
+    @setpoint.setter
+    def setpoint(self, val):
+        setpoint = str(val)
+        self.post(json=dict(sn=self.sn, par=[[125, 7, "0"], [2, 2, "1"], [5, 1, setpoint]]))
+
+    @staticmethod
+    def get_setpoint(data):
+        return float(data['t.5']) / 16
+
+    @property
+    def mode(self):
+        """
+        Returns the current mode of the thermostat.
+        Returns
+        -------
+        mode : `int`
+            `0` for schedule mode, `3` for manual mode and `4` for away mode.
+        """
+        if self._mode is None:
+            data = self.status()
+            if data:
+                self._mode = self.get_mode(data)
+        return self._mode
+
+    def get_mode(self, data):
+        if 'f.16' in data:  # in firmware 2.4 was added new flag for power off/on
+            is_on = int(data['f.16']) == 0
+        else:
+            is_on = self.is_on()
+
+        if not is_on:
+            return -1
+        else:
+            return int(data['m.1'])
+
+    @mode.setter
+    def mode(self, val):
+        val = int(val)
+        if val not in [0, 1]:
+            raise ValueError("mode must be either 0,1")
+
+        self.post(json=dict(sn=self.sn, par=[[125, 7, "0"], [2, 2, str(val)]]))
+
+    @property
+    def state(self):
+        """
+        Current state of the relay.
+        Returns
+        -------
+        state : `bool`
+            returns `True` if the relay is on and `False` if the relay is off.
+        """
+        if self._state is None:
+            data = self.status()
+            if data:
+                self._state = self.get_state(data)
+        return self._state
+
+    @staticmethod
+    def get_state(data):
+        return int(data['f.0']) == 1
+
+    def turn_on(self):
+        return self.post(json=dict(sn=self.sn, par=[[125, 7, "0"]]))
+
+    def turn_off(self):
+        return self.post(json=dict(sn=self.sn, par=[[125, 7, "1"]]))
+
+    def update(self):
+        data = self.status()
+        if data:
+            self._setpoint = self.get_setpoint(data)
+            self._temperature = self.get_temperature(data)
+            self._mode = self.get_mode(data)
+            self._state = self.get_state(data)
